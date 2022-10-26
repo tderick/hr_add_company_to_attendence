6e3f0e474750afeae36c4892b938d0fe24f0fadf
TEMFACK DERICK <dericktemfack@gmail.com>
2022-10-26 10:13:15 +0100

    Overwrite create and write method

--- models/attendance.py
+++ models/attendance.py
@@ -4,8 +4,16 @@ from odoo import fields, models, api
 class InheritedAttendance(models.Model):
     _inherit = "hr.attendance"
 
-    agence = fields.Char(string="Agence", compute="_compute_agence")
+    agence = fields.Char(string="Agence", readonly=True)
 
-    def _compute_agence(self):
-        for attendance in self:
-            attendance.agence = self.env.user.company_id.street
+    @api.model
+    def create(self, values):
+        self.agence = self.env.user.company_id.street
+        override_create = super(InheritedAttendance, self).create(values)
+        return override_create
+
+    @api.multi
+    def write(self, values):
+        self.agence = self.env.user.company_id.street
+        override_write = super(InheritedAttendance, self).write(values)
+        return override_write
