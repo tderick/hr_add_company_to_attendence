670cda4b82b5fe17bc7bd22e772a21a8de1f35c4
TEMFACK DERICK <dericktemfack@gmail.com>
2022-10-27 04:55:38 +0100

    save

--- models/attendance.py
+++ models/attendance.py
@@ -8,8 +8,7 @@ class InheritedAttendance(models.Model):
 
     @api.model
     def create(self, values):
-        for attendance in self:
-            attendance.agence = self.env.company_id.name
+        self.agence = self.env.company_id.name
         override_create = super(InheritedAttendance, self).create(values)
         return override_create
 
